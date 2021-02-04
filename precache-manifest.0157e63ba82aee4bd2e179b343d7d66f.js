21d3317cdcd5e08577375974f05381fc46157b7d
Abdulwasiu Abdulmuize <AOMuiz@users.noreply.github.com>
2021-02-04 22:25:01 +0100

    Updates

---
+++ precache-manifest.0157e63ba82aee4bd2e179b343d7d66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cbedef4af0bb5790865e96d9f31f8b",
+    "url": "/cov19-tracker/index.html"
+  },
+  {
+    "revision": "aec79b6ad7eac3b4dedf",
+    "url": "/cov19-tracker/static/css/main.1441446b.chunk.css"
+  },
+  {
+    "revision": "e8efa20bb2c133cddcad",
+    "url": "/cov19-tracker/static/js/2.27ed506c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cov19-tracker/static/js/2.27ed506c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec79b6ad7eac3b4dedf",
+    "url": "/cov19-tracker/static/js/main.97b6dfa7.chunk.js"
+  },
+  {
+    "revision": "83cce6d83ace13341056",
+    "url": "/cov19-tracker/static/js/runtime-main.4298d469.js"
+  }
+]);
