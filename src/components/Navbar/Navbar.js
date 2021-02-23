2c30e42fb47bc247dc701beb0324df8a4d56ee9e
Abdulwasiu Abdulmuize <54303765+AOMuiz@users.noreply.github.com>
2021-02-23 08:15:24 +0100

    update Navbar and remove comment error

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,28 +4,6 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    // <header id='nav-header'>
-    //   <div className='welcome-banner'>
-    //     <h3>Covid-19 Statistics</h3>
-    //   </div>
-    //   <nav id='navbar'>
-    //     <ul>
-    //       <Link to='/Home'>
-    //         <li className='nav-link'>Home</li>
-    //       </Link>
-    //       <Link to='/News'>
-    //         <li className='nav-link'>News</li>
-    //       </Link>
-    //       <Link to='/VaccineNews'>
-    //         <li className=''>Vaccine</li>
-    //       </Link>
-    //       <Link to='/Map'>
-    //         <li className=''>Map</li>
-    //       </Link>
-    //     </ul>
-    //   </nav>
-    // </header>
-
     <header>
       <div className="bg-img">
         <nav className="topnav" id="myTopnav">
